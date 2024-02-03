623eed31c777fe0d7febb5228a2a18d25aa682f1
Mbd <bendaya.mehdi@gmail.com>
2024-02-03 13:39:27 -0500

    V1

---
+++ Fonctions.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+# =============================================================================
+# =============================================================================
+# =====================  Premier cas : Régime permanent  ======================
+# =============================================================================
+# =============================================================================
+
+def C_analytique(prm):
+        
+        """ Fonction qui calcule f(x) pour le premier cas
+        Entrée : 
+        - x : vecteur de position 
+        Sortie :
+        - y : vecteur contenant la valeur numérique de la fonction """
+        r = np.linspace(0, prm.R, prm.n)
+        y = (0.25*(prm.S/prm.D_eff)*prm.R*prm.R*(((r*r)/(prm.R*prm.R))-1))+prm.Ce
+        
+        return r,y
+ 
+
+# =============================================================================
+# =============================================================================
+# =================  Premier cas : A ==========================================
+# =============================================================================
+# =============================================================================
+
+
+def PbB(prm):
+    """ Fonction qui résout le systeme  pour le deuxième cas
+    Entrées:
+        - prm : vecteur contenant la position 
+
+    Sorties :
+        - c : Matrice (array) qui contient la solution numérique
+        - tps : vecteur (liste) qui contient les différents temps de résolution"""        
+
+    dr = prm.dr #Pas en espace
+    dt = prm.dt
+    D_eff=prm.D_eff
+    n  = prm.n
+    r = np.linspace(0, prm.R, n) #Discrétisation en espace
+    A = np.zeros([prm.n, prm.n]) #Matrice A
+    b = np.zeros(prm.n) #Vecteur b
+    t=0   
+    tps=[0]
+    err_t_tdt=10
+    # Remplissage du centre de la matrice A et du vecteur b
+    c_t=C_analytique(prm)[1]
+    # b=7
+    # a=(prm.Ce-b)/prm.R
+    # c_t[:-1] = [a*r[i]+b for i in range(n-1)]
+    # c_t[-1]=prm.Ce
+    c=[c_t]
+    # Remplissage du centre de la matrice A et du vecteur b    
+    for i in range(1, prm.n-1):
+        A[i, i+1] = -dt*D_eff*(1/((r[i]*dr))+1/dr**2)
+        A[i, i] = 1+D_eff*dt*(1/(r[i]*dr)+2/dr**2)
+        A[i, i-1] = -dt*D_eff*(1/dr**2)
+    
+    A[-1, -1] = 1
+    A[0, 0] = -3
+    A[0, 1] = 4
+    A[0, 2] = -1
+    
+    
+    while err_t_tdt>prm.err_t_tdt:
+        
+        b = np.zeros(n)
+        b[1:n-1]=-dt*prm.S+c_t[1:n-1]
+        b[0] = 0
+        b[-1] = prm.Ce
+
+        # Résolution du système matriciel
+        c_tdt = np.linalg.solve(A, b)
+        err_t_tdt=np.linalg.norm(c_t-c_tdt)
+        c_t[:]=c_tdt[:]
+        t+=prm.dt
+        tps.append(t)
+    return c_tdt,tps
+
+def PbF(prm):
+    """ Fonction qui résout le systeme  pour le deuxième cas
+    Entrées:
+        - prm : vecteur contenant la position 
+
+    Sorties :
+        - c : Matrice (array) qui contient la solution numérique
+        - tps : vecteur (liste) qui contient les différents temps de résolution"""        
+
+    dr = prm.dr #Pas en espace
+    dt = prm.dt
+    D_eff=prm.D_eff
+    n  = prm.n
+    r = np.linspace(0, prm.R, n) #Discrétisation en espace
+    A = np.zeros([prm.n, prm.n]) #Matrice A
+    b = np.zeros(prm.n) #Vecteur b
+    t=0   
+    tps=[0]
+    err_t_tdt=10
+    # Remplissage du centre de la matrice A et du vecteur b
+    c_t=np.ones(n)
+    b=7
+    a=(prm.Ce-b)/prm.R
+    c_t = C_analytique(prm)[1]
+
+    # Remplissage du centre de la matrice A et du vecteur b    
+    for i in range(1, prm.n-1):
+        A[i, i+1] = -dt*D_eff*(1/((2*r[i]*dr))+1/dr**2)
+        A[i, i] = 1+D_eff*dt*(2/dr**2)
+        A[i, i-1] = -dt*D_eff*((1/dr**2)-1/((2*r[i]*dr)))
+    
+    A[-1, -1] = 1
+    A[0, 0] = -3
+    A[0, 1] = 4
+    A[0, 2] = -1
+    
+    while err_t_tdt>prm.err_t_tdt:
+        b = np.zeros(n)
+        b[1:n-1]=-dt*prm.S+c_t[1:n-1]
+        b[0] = 0
+        b[-1] = prm.Ce
+        
+        # Résolution du système matriciel
+        c_tdt = np.linalg.solve(A, b)
+        err_t_tdt=np.linalg.norm(c_t-c_tdt)
+        c_t[:]=c_tdt[:]
+        t+=prm.dt
+        tps.append(t)
+
+    return c_tdt,tps
---
+++ Projet.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import pytest
+from Fonctions import *
+
+
+# Assignation des paramètres pour le premier cas
+class param():
+    S=8*10**-9
+    D=1
+    R=D/2
+    Ce=12
+    D_eff=10**-10
+    dr=0.0001
+    dt=0.00001
+    n  = int(R/dr)
+    err_t_tdt=0.000000001
+    
+    
+# prmPB=prm()    
+# plt.figure() 
+# plt.grid()   
+# r,C_analy=C_analytique(prmPB) 
+# plt.plot(r,C_analy) 
+
+# C_num,tps=PbB(prmPB)
+# plt.plot(r,C_num) 
+  
+
+# # =============================================================================
+    
+
+
+# =============================================================================
+# =============================================================================
+# =====================  Premier cas : Schema d'ordre 1  ======================
+# =============================================================================
+# =============================================================================  
+    
+# def PartieE():    
+     
+    
+# Initialisation des paramètres (Valeurs qui peuvent être modifiées)
+prm = param()
+dr_testee = [0.1,0.05,0.025,0.01,0.005,0.0025,0.001,0.0005,0.00025,0.0001,0.00005,0.00001]   
+  
+  
+# Initialisation des vecteurs
+erreur_L1 = []
+erreur_L2 = []
+erreur_Linf = []
+   
+# Calcul de de l'erreur
+for dr_act in dr_testee:
+   
+    prm.dr = dr_act
+    
+    r,C_analy=C_analytique(prm)
+    C_num,tps=PbB(prm)
+    
+    epsilon_h=C_num-C_analy
+    
+    erreur_L1.append(np.sum(abs(epsilon_h))/prm.n) # Calcul de l'erreur
+    erreur_L2.append(np.linalg.norm(epsilon_h))
+    erreur_Linf.append(max(abs(epsilon_h)))
+    
+plt.figure()
+plt.rcParams['text.usetex'] = True
+plt.loglog(dr_testee,erreur_L1,'.')
+plt.gca().invert_xaxis()
+plt.grid()
+plt.xlabel("dr")
+plt.xlabel("Erreur L1")
+plt.title("Convergence de l'erreur L1 en fonction de dr")
+
+plt.figure()
+plt.rcParams['text.usetex'] = True
+plt.loglog(dr_testee,erreur_L2,'.')
+plt.gca().invert_xaxis()
+plt.grid()
+plt.xlabel("dr")
+plt.xlabel("Erreur L2")
+plt.title("Convergence de l'erreur L2 en fonction de dr")
+print(dr_testee,erreur_L2)
+
+plt.figure()
+plt.rcParams['text.usetex'] = True
+plt.loglog(dr_testee,erreur_Linf,'.')
+plt.gca().invert_xaxis()
+plt.grid()
+plt.xlabel("dr")
+plt.xlabel("Erreur Linf")
+plt.title("Convergence de l'erreur $L_{\infty}$ en fonction de dr")
+
+
+# =============================================================================
+# =============================================================================
+# =====================  Premier cas : Schema d'ordre 2  ======================
+# =============================================================================
+# =============================================================================  
+    
+def PartieE():    
+     
+    
+     # Initialisation des paramètres (Valeurs qui peuvent être modifiées)
+     prm = param()
+     dr_testee = [0.1,0.05,0.025,0.01,0.005,0.0025,0.001,0.0005,0.00025,0.0001,0.00005,0.00001]   
+   
+   
+     # Initialisation des vecteurs
+     erreur_L1 = []
+     erreur_L2 = []
+     erreur_Linf = []
+    
+     # Calcul de de l'erreur
+     for dr_act in dr_testee:
+        
+         prm.dr = dr_act
+         
+         r,C_analy=C_analytique(prm)
+         C_num,tps=PbB(prm)
+         print(tps)
+         epsilon_h=C_num-C_analy
+         
+         erreur_L1.append(np.sum(abs(epsilon_h))/prm.n) # Calcul de l'erreur
+         erreur_L2.append(np.linalg.norm(epsilon_h))
+         erreur_Linf.append(max(abs(epsilon_h)))
+         
+     plt.figure()
+     plt.rcParams['text.usetex'] = True
+     plt.loglog(dr_testee,erreur_L1,'.')
+     plt.gca().invert_xaxis()
+     plt.grid()
+     plt.xlabel("dr")
+     plt.xlabel("Erreur L1")
+     plt.title("Convergence de l'erreur L1 en fonction de dr")
+     
+     plt.figure()
+     plt.rcParams['text.usetex'] = True
+     plt.loglog(dr_testee,erreur_L2,'.')
+     plt.gca().invert_xaxis()
+     plt.grid()
+     plt.xlabel("dr")
+     plt.xlabel("Erreur L2")
+     plt.title("Convergence de l'erreur L2 en fonction de dr")
+     
+     plt.figure()
+     plt.rcParams['text.usetex'] = True
+     plt.loglog(dr_testee,erreur_Linf,'.')
+     plt.gca().invert_xaxis()
+     plt.grid()
+     plt.xlabel("dr")
+     plt.xlabel("Erreur Linf")
+     plt.title("Convergence de l'erreur $L_{\infty}$ en fonction de dr") 
+
+
+# print("Veuillez attendre la vérification du code est en cours.")    
+# pytest.main(['-q', '--tb=long', 'Tests.py'])    
