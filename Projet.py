a873bea8d629b8a7f93a1fd18c1a057e9ace54d7
Mbd <bendaya.mehdi@gmail.com>
2024-02-10 11:32:27 -0500

    V4.1
    3.91/10

--- Projet.py
+++ Projet.py
@@ -1,34 +1,41 @@
 # -*- coding: utf-8 -*-
-
+''' Ce code permet de resoudre l'equation de diffusion de sel dans le beton.
+Le projet ce divise 4 sous-programme:
+    - PartieE() : Schema d'ordre 1 en resolvant le systeme transitoire 
+    - PartieE_S() : Schema d'ordre 1 en resolvant le systeme stationnaire
+    - PartieF() : Schema d'ordre 2 en resolvant le systeme transitoire 
+    - PartieF_S() : Schema d'ordre 2 en resolvant le systeme stationnaire
+    '''
+from os import environ
+from time import time
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import pytest
 from Fonctions import *
-from os import environ
+
+
 N_THREADS = '12'
 environ['OMP_NUM_THREADS'] = N_THREADS
 environ['OPENBLAS_NUM_THREADS'] = N_THREADS
 environ['MKL_NUM_THREADS'] = N_THREADS
 environ['VECLIB_MAXIMUM_THREADS'] = N_THREADS
 environ['NUMEXPR_NUM_THREADS'] = N_THREADS
-from time import time
+
 start = time()
 
-# Assignation des paramètres pour le premier cas
+
+'''Creation d'une classe qui servira pour tous les cas  La classe pourra etre modifier au besoin'''
 class param():
-    S=8*10**-9
-    D=1
-    R=D/2
-    Ce=12
-    D_eff=10**-10
-    dr=0.0001
-    dt=0.5*dr**2/(D_eff*10)
-    n  = int(R/dr)
-    err_t_tdt=10**-9
+    S=8*10**-9 #Terme source
+    D=1        #Diametre de la colonne
+    R=D/2      #Rayon de la colonne
+    Ce=12      #Concentration en sel de l'eau
+    D_eff=10**-10 #Coefficient de diffusion du sel dans le beton
+    dr=0.0001  #Pas en espace
+    dt=0.5*dr**2/(D_eff*10) # Pas en temps
+    n  = int(R/dr) #Nombre de noeuds
+    err_t_tdt=10**-9 #Condition d'arret        
     
-    
-# prmPB=param()    
+'''prmPB=param()    
 # plt.figure() 
 # plt.grid()   
 # r,C_analy=C_analytique(prmPB) 
@@ -38,13 +45,10 @@ class param():
 # plt.plot(r,C_num) 
   
 # duration = time() - start
-# print(f'Took {duration:.3f} seconds')
-
-# # ===========================================================================
-    
+# print(f'Took {duration:.3f} seconds')'''
 
 
-# =============================================================================
+'''# ==========================================================================
 # =============================================================================
 # =====================  Premier cas : Schema d'ordre 1  ======================
 # =============================================================================
@@ -52,11 +56,11 @@ class param():
  
 # ============================================================================= 
 # ==============================Regime transitoire=============================
-# =============================================================================    
-def PartieE():    
+# =========================================================================='''  
+def partieE():    
      
     
-    # Initialisation des paramètres (Valeurs qui peuvent être modifiées)
+    '''Initialisation des paramètres (Valeurs qui peuvent être modifiées)'''
     prm = param()
     dr_testee = [0.1,0.05,0.025,0.01,0.009,0.008,0.007,0.006,0.005]   
       
@@ -80,7 +84,7 @@ def PartieE():
         
         epsilon_h=C_num-C_analy
         
-        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n) # Calcul de l'erreur
+        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n)
         erreur_L2.append(np.linalg.norm(epsilon_h))
         erreur_Linf.append(max(abs(epsilon_h)))
         
@@ -115,9 +119,9 @@ def PartieE():
     plt.title("Convergence de l'erreur $L_{\infty}$ en fonction de dr")
     
 
-# ============================================================================= 
+'''# ==========================================================================
 # ==============================Regime stationnaire============================
-# =============================================================================    
+# =========================================================================='''
 def PartieE_S():    
      
     
@@ -146,7 +150,7 @@ def PartieE_S():
         
         epsilon_h=C_num-C_analy
         
-        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n) # Calcul de l'erreur
+        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n)
         erreur_L2.append(np.linalg.norm(epsilon_h))
         erreur_Linf.append(max(abs(epsilon_h)))
         
@@ -185,34 +189,36 @@ def PartieE_S():
     plt.figure()
     for i in range(len(c_ordre1)):
         if dr_testee[i] in [0.1,0.01,0.001,0.0001,0.00001]:
-          lab='dr='+str(dr_testee[i])
-          plt.plot(r_ordre1[i],c_ordre1[i],'-.',label=lab)
+            lab='dr='+str(dr_testee[i])
+            plt.plot(r_ordre1[i],c_ordre1[i],'-.',label=lab)
     plt.legend()
     plt.grid()
     plt.xlabel("dr")
     plt.ylabel("C")
     plt.title("Profil de concentration en fonction de dr") 
+    
+'''# ==========================================================================
 # =============================================================================
+# =====================  Deuxieme cas : Schema d'ordre 2  =====================
 # =============================================================================
-# =====================  Deuxieme cas : Schema d'ordre 2  ======================
-# =============================================================================
-# =============================================================================  
+# =========================================================================='''  
     
 def PartieF():    
      
     
-     # Initialisation des paramètres (Valeurs qui peuvent être modifiées)
+    '''Initialisation des paramètres (Valeurs qui peuvent être modifiées)'''
     prm = param()
     dr_testee = [0.1,0.05,0.025,0.01]   
       
       
-    # Initialisation des vecteurs
+    '''Initialisation des vecteurs'''
     erreur_L1 = []
     erreur_L2 = []
     erreur_Linf = []
     c_ordre2= []
     r_ordre2= []   
-    # Calcul de de l'erreur
+    
+    ''''Calcul de de l'erreur'''
     for dr_act in dr_testee:
        
         prm.dr = dr_act
@@ -226,7 +232,7 @@ def PartieF():
         
         epsilon_h=C_num-C_analy
         
-        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n) # Calcul de l'erreur
+        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n)
         erreur_L2.append(np.linalg.norm(epsilon_h))
         erreur_Linf.append(max(abs(epsilon_h)))
         
@@ -264,9 +270,9 @@ def PartieF():
     
     plt.figure()
     for i in range(len(c_ordre2)):
-       if dr_testee[i] in [0.1,0.01,0.001,0.0001,0.00001]:
-         lab='dr='+str(dr_testee[i])
-         plt.plot(r_ordre2[i],c_ordre2[i],'-.',label=lab)
+        if dr_testee[i] in [0.1,0.01,0.001,0.0001,0.00001]:
+            lab='dr='+str(dr_testee[i])
+            plt.plot(r_ordre2[i],c_ordre2[i],'-.',label=lab)
     plt.legend()
     plt.grid()
     plt.xlabel("dr")
@@ -274,15 +280,15 @@ def PartieF():
     plt.title("Profil de concentration en fonction de dr") 
     
 
-# ============================================================================= 
+'''# ==========================================================================
 # ==============================Regime stationnaire============================
-# =============================================================================    
+# =========================================================================='''  
 def PartieF_S():    
      
     
      # Initialisation des paramètres (Valeurs qui peuvent être modifiées)
     prm = param()
-    dr_testee = [0.1,0.05,0.025,0.01,0.005,0.0025,0.001,0.0005,0.00025,0.0001]   
+    dr_testee = [0.1,0.05,0.025,0.01,0.005,0.0025,0.001,0.0005,0.00025,0.0001,0.00005,0.000025]   
       
       
     # Initialisation des vecteurs
@@ -305,7 +311,7 @@ def PartieF_S():
         
         epsilon_h=C_num-C_analy
         
-        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n) # Calcul de l'erreur
+        erreur_L1.append(np.sum(abs(epsilon_h))/prm.n)
         erreur_L2.append(np.linalg.norm(epsilon_h))
         erreur_Linf.append(max(abs(epsilon_h)))
         
@@ -346,14 +352,11 @@ def PartieF_S():
         if dr_testee[i] in [0.1,0.01,0.001,0.0001,0.00001]:
             
     # plt.rcParams['text.usetex'] = True
-          lab='dr='+str(dr_testee[i])
-          plt.plot(r_ordre2[i],c_ordre2[i],'-.',label=lab)
+            lab='dr='+str(dr_testee[i])
+            plt.plot(r_ordre2[i],c_ordre2[i],'-.',label=lab)
     plt.legend()
     plt.grid()
     plt.xlabel("dr")
     plt.ylabel("C")
-    plt.title("Profil de concentration en fonction de dr") 
-        
-
-# print("Veuillez attendre la vérification du code est en cours.")    
-# pytest.main(['-q', '--tb=long', 'Tests.py'])    
+    plt.title("Profil de concentration en fonction de dr")
+    
